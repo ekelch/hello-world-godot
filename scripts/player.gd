f1f83de6da3fc2124500bb2fa986a76e63ced69b
Evan <evan.c.kelch@gmail.com>
2025-02-27 21:53:10 -0500

    added some sfx on jump and die

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 const SPEED = 130
 const JUMP_VELOCITY = -280
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
 
 
 func _physics_process(delta: float) -> void:
@@ -14,6 +15,7 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
 	# velocity, direction
 	var direction := Input.get_axis("move_left", "move_right")
--- scripts/slime.gd
+++ scripts/slime.gd
@@ -5,6 +5,7 @@ var direction = 1
 
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var ray_cast_right: RayCast2D = $RayCastRight
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -14,3 +15,7 @@ func _process(delta: float) -> void:
 		direction = -1
 	$AnimatedSprite2D.flip_h = direction < 0
 	position.x += direction * velocity * delta
+
+
+func _on_killzone_body_entered(body: Node2D) -> void:
+	audio_stream_player_2d.play()
